5dab3788380df5d583b229cdc5039bc8391c1237
danielalvesmoura <danielalvesmoura2006@gmail.com>
2026-04-11 18:23:31 -0300

    feat-finalizado-card-do-github

---
+++ js/github.js
@@ -0,0 +1,25 @@
+const username = "danielalvesmoura";
+
+// 🔹 Perfil
+fetch(`https://api.github.com/users/${username}`).then(res => res.json()).then(data => {
+    document.getElementById("name").textContent = data.name || data.login;
+    document.getElementById("followers").textContent = data.followers;
+    document.getElementById("repos-count").textContent = data.public_repos;
+});
+
+// 🔹 Repositórios
+fetch(`https://api.github.com/users/${username}/repos`).then(res => res.json()).then(repos => {
+    const list = document.getElementById("repos");
+
+    repos.sort((a, b) => b.stargazers_count - a.stargazers_count).slice(0, 6).forEach(repo => {
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+          <a class="repo_link" href="${repo.html_url}" target="_blank">
+            ${repo.name}
+          </a>
+        `;
+
+        list.appendChild(li);
+    });
+});
